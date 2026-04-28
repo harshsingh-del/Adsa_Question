a8c6de6980de07796bd121b632a0a7bc1931ce0a
Harsh Singh <harshsinghrajput895@gmail.com>
2026-04-28 15:27:23 +0530

    Time: 36 ms (11.64%) | Memory: 47.6 MB (10.12%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,14 +1,25 @@
 class Solution {
-    public int coinChange(int[] coins, int amt) {
-        int[] dp=new int[amt+1];
-        Arrays.fill(dp,amt+1);
-        dp[0]=0;
-        
-        for(int coin:coins){
-            for(int j=coin;j<=amt;j++){
-                dp[j]=Math.min(dp[j],1+dp[j-coin]);
-            }
-        }
-        return dp[amt]>amt?-1:dp[amt];
+    static int coin(int i,int [] coins,int amount,int [][] dp){
+        if(amount==0) return 0;
+        if(i==coins.length) return Integer.MAX_VALUE;
+        if(dp[i][amount]!=-1) return  dp[i][amount];
+        int take=Integer.MAX_VALUE;
+       if(coins[i]<=amount) {
+           int res=coin(i,coins,amount-coins[i],dp);
+           if(res!=Integer.MAX_VALUE){
+            take= 1+res;
+           }
+       }
+        int skip=coin(i+1,coins,amount,dp);
+        dp[i][amount]= Math.min(take,skip);
+        return dp[i][amount];
     }
-}
+    public int coinChange(int[] coins, int amount) {
+        int [][] dp= new int[coins.length+1][amount+1];
+        for(int[] arr:dp){
+            Arrays.fill(arr,-1);
+        }
+       int ans=coin(0,coins,amount,dp);   
+       return ans== Integer.MAX_VALUE ? -1:ans;
+    }
+} 
