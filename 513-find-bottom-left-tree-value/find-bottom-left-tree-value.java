e0e38f17e3ab793da9d87b5df883281a0f6915bd
Harsh Singh <harshsinghrajput895@gmail.com>
2026-05-08 16:12:24 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (11.31%) - LeetSync

---
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int ans=0;
+    int maxdepth=-1;
+
+    public int findBottomLeftValue(TreeNode root) {
+        dfs(root,0);
+        return ans;
+    }
+    public void dfs(TreeNode node, int depth){
+        if(node==null) return;
+        if(depth>maxdepth){
+            maxdepth=depth;
+            ans=node.val;
+        }
+        dfs(node.left,depth+1);
+        dfs(node.right,depth+1);
+    }
+}
