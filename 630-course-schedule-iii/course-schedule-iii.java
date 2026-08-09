686e731fcdc0fb6781aa381b94e2f7c6e8ee8f3a
Harsh Singh <harshsinghrajput895@gmail.com>
2026-08-09 23:01:06 +0530

    Time: 50 ms (29.46%) | Memory: 57.9 MB (5.21%) - LeetSync

---
+++ 630-course-schedule-iii/course-schedule-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int time = 0;
+
+        for (int[] c : courses) {
+            time += c[0];
+            pq.add(c[0]);
+
+            if (time > c[1]) {
+                time -= pq.poll();
+            }
+        }
+
+        return pq.size();
+    }
+}
